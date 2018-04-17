3f204d55e04393d98d2716c7cc61622206023ca4
Andrey <arakelyants.andrey@yandex.ru>
2018-04-17 18:11:47 +0300

    Complete readme and requirements.

--- README.md
+++ README.md
@@ -1,6 +1,44 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+This script evaluates a strength of password from 1 to 10 points.
+
+**Evaluation policies**
+
+| Criterion | Mark |
+| --------- | ---- |
+| Upper and lower case letters | +2 |
+| Digits | +2 |
+| Special characters | +2 |
+| Underlines, minuses and brackets | +2 |
+| Password length > 8 symbols | +1 |
+| Password length > 12 symbols | +2 |
+| More than 4 numbers in a row | -1 |
+| Date and time | -1 |
+| Password from blacklist | -1 | 
+
+# Quickstart
+
+The script requires Python v3.5.
+
+*Requirements:*
+- dateparser==0.7.0
+- python-dateutil==2.7.2
+- pytz==2018.4
+- regex==2018.2.21
+- six==1.11.0
+- tzlocal==1.5.1
+
+
+A path of file containing a text is the optional argument of the script. 
+
+To run script on Linux:
+```
+$ python password_strength.py -b password_blacklist.txt
+Password: ******
+Your password gets 1 point(s) out of 10.
+```
+
+Windows usage is the same.
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -37,8 +37,11 @@ def check_special_characters(password):
 def check_password_blacklist(password, blacklist_file=None):
     if not blacklist_file:
         return False
-    with open(blacklist_file, 'r', encoding='utf-8') as blacklist_file:
-        blacklist = blacklist_file.read()
+    try:
+        with open(blacklist_file, 'r', encoding='utf-8') as blacklist_file:
+            blacklist = blacklist_file.read()
+    except FileNotFoundError:
+        return None
     if password in blacklist:
         return True
     return False
@@ -67,7 +70,6 @@ def check_underlines_minuses_brackets(password):
 
 
 def check_date(password):
-    print(search_dates(password))
     if search_dates(password):
         return True
     return False
@@ -91,7 +93,10 @@ def check_bad_features(password_points, password, blacklist_path):
         password_points -= 1
     if check_date(password):
         password_points -= 1
-    if check_password_blacklist(password, blacklist_path):
+    password_in_blacklist = check_password_blacklist(password, blacklist_path)
+    if password_in_blacklist is None:
+        return None
+    if password_in_blacklist:
         password_points -= 1
     return password_points
 
@@ -104,6 +109,10 @@ def get_password_strength(password, blacklist_path=None):
         password,
         blacklist_path
     )
+    if password_strength < 1:
+        password_strength = 1
+    if password_strength > 10:
+        password_strength = 10
     return password_strength
 
 
@@ -111,5 +120,11 @@ if __name__ == '__main__':
     console_arguments = get_console_arguments()
     password_blacklist_file = console_arguments.b
     user_password = getpass.getpass(prompt='Password: ')
-    password_score = get_password_strength(user_password, password_blacklist_file)
-    print('Your password get {} points out of 10.'.format(password_score))
+    password_score = get_password_strength(
+        user_password,
+        password_blacklist_file
+    )
+    if password_score is None:
+        print('Can not find the file containing a password blacklist.')
+    else:
+        print('Your password gets {} point(s) out of 10.'.format(password_score))
