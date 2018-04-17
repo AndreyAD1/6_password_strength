c4fcf1f5109973f24a87c9d96107630988b1c793
Andrey <arakelyants.andrey@yandex.ru>
2018-04-17 17:41:10 +0300

    Complete the script.

--- password_strength.py
+++ password_strength.py
@@ -15,50 +15,39 @@ def get_console_arguments():
     return args
 
 
-def check_all_digits(password):
-    all_digits = password.isdigit()
-    return all_digits
-
-
 def check_case_sensitivity(password):
-    # if check_all_digits(password):
-    #     return False
-    # case_sensitivity = not(password.islower() or password.isupper())
-    # print(case_sensitivity)
-    lower_letter = any(letter.islower() for letter in password)
-
-
-    return True
+    lower_letter = any(symbol.islower() for symbol in password)
+    upper_letter = any(symbol.isupper() for symbol in password)
+    case_sensitivity = lower_letter and upper_letter
+    return case_sensitivity
 
 
 def check_digits(password):
-    if check_all_digits(password):
+    if password.isdigit():
         return False
-    if re.search(r'\d', password):
-        return True
-    return False
+    digit = any(symbol.isdigit() for symbol in password)
+    return digit
 
 
 def check_special_characters(password):
     if re.search(r'\W', password):
         return True
-    # \s - whitespace
 
 
 def check_password_blacklist(password, blacklist_file=None):
     if not blacklist_file:
-        return
+        return False
     with open(blacklist_file, 'r', encoding='utf-8') as blacklist_file:
         blacklist = blacklist_file.read()
     if password in blacklist:
-        return False
-    return True
+        return True
+    return False
 
 
 def check_common_numbers(password):
     if re.search(r'\d{4+}', password):
-        return False
-    return True
+        return True
+    return False
 
 
 def check_password_length(password):
@@ -67,7 +56,7 @@ def check_password_length(password):
     if password_length >= 8:
         improvement_by_password_length += 1
     if password_length >= 12:
-        improvement_by_password_length += 2
+        improvement_by_password_length += 1
     return improvement_by_password_length
 
 
@@ -79,35 +68,48 @@ def check_underlines_minuses_brackets(password):
 
 def check_date(password):
     print(search_dates(password))
-    if not search_dates(password):
+    if search_dates(password):
         return True
     return False
 
 
-def get_password_strength(password, blacklist_path = None):
-    password_strength = 1
+def check_good_features(password_points, password):
     if check_case_sensitivity(password):
-        password_strength += 1
+        password_points += 2
     if check_digits(password):
-        password_strength += 1
+        password_points += 2
     if check_special_characters(password):
-        password_strength += 1
-    if check_password_blacklist(password, blacklist_path):
-        password_strength += 1
-    if check_common_numbers(password):
-        password_strength += 1
+        password_points += 2
     if check_underlines_minuses_brackets(password):
-        password_strength += 1
+        password_points += 1
+    password_points = password_points + check_password_length(password)
+    return password_points
+
+
+def check_bad_features(password_points, password, blacklist_path):
+    if check_common_numbers(password):
+        password_points -= 1
     if check_date(password):
-        password_strength += 1
-    password_strength = password_strength + check_password_length(password)
+        password_points -= 1
+    if check_password_blacklist(password, blacklist_path):
+        password_points -= 1
+    return password_points
+
+
+def get_password_strength(password, blacklist_path=None):
+    password_strength = 1
+    password_strength = check_good_features(password_strength, password)
+    password_strength = check_bad_features(
+        password_strength,
+        password,
+        blacklist_path
+    )
     return password_strength
 
 
 if __name__ == '__main__':
     console_arguments = get_console_arguments()
     password_blacklist_file = console_arguments.b
-    # user_password = console_arguments.password
     user_password = getpass.getpass(prompt='Password: ')
     password_score = get_password_strength(user_password, password_blacklist_file)
     print('Your password get {} points out of 10.'.format(password_score))
