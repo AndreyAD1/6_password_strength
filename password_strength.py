d06f4c7408d4a82f995b6bffac4c036a1d16dc4a
Andrey <arakelyants.andrey@yandex.ru>
2018-04-18 12:58:40 +0300

    Add bool expresions to the functions

--- password_strength.py
+++ password_strength.py
@@ -9,6 +9,7 @@ def get_console_arguments():
     parser.add_argument(
         '-b',
         '-password_blacklist',
+        default=None,
         help='enter path of a file containing password blacklist'
     )
     args = parser.parse_args()
@@ -30,50 +31,37 @@ def check_digits(password):
 
 
 def check_special_characters(password):
-    if re.search(r'\W', password):
-        return True
-    return False
+    return bool(re.search(r'\W', password))
 
 
 def check_underlines_minuses_brackets(password):
-    if re.search(r'[()_-]', password):
-        return True
-    return False
+    return bool(re.search(r'[()_-]', password))
 
 
 def check_password_length(password):
     improvement_by_password_length = 0
     password_length = len(password)
-    if password_length >= 8:
+    reliable_password_length = 8
+    very_reliable_password_length = 12
+    if password_length >= reliable_password_length:
         improvement_by_password_length += 1
-    if password_length >= 12:
+    if password_length >= very_reliable_password_length:
         improvement_by_password_length += 1
     return improvement_by_password_length
 
 
 def check_common_numbers(password):
-    if re.search(r'\d{4+}', password):
-        return True
-    return False
+    return bool(re.search(r'\d{4+}', password))
 
 
 def check_date(password):
-    if search_dates(password):
-        return True
-    return False
+    return bool(search_dates(password))
 
 
-def check_password_blacklist(password, blacklist_file=None):
-    if not blacklist_file:
+def check_password_blacklist(password, blacklist):
+    if not blacklist:
         return False
-    try:
-        with open(blacklist_file, 'r', encoding='utf-8') as blacklist_file:
-            blacklist = blacklist_file.read()
-    except FileNotFoundError:
-        return None
-    if password in blacklist:
-        return True
-    return False
+    return password in blacklist
 
 
 def check_good_features(password_points, password):
@@ -89,45 +77,53 @@ def check_good_features(password_points, password):
     return password_points
 
 
-def check_bad_features(password_points, password, blacklist_path):
+def check_bad_features(password_points, password, blacklist):
     if check_common_numbers(password):
         password_points -= 1
     if check_date(password):
         password_points -= 1
-    password_in_blacklist = check_password_blacklist(password, blacklist_path)
-    if password_in_blacklist is None:
-        return None
+    password_in_blacklist = check_password_blacklist(password, blacklist)
     if password_in_blacklist:
         password_points -= 1
     return password_points
 
 
-def get_password_strength(password, blacklist_path=None):
+def get_password_strength(password, blacklist):
     password_strength = 1
     password_strength = check_good_features(password_strength, password)
     password_strength = check_bad_features(
         password_strength,
         password,
-        blacklist_path
+        blacklist
     )
-    if password_strength < 1:
-        password_strength = 1
-    if password_strength > 10:
-        password_strength = 10
+    if password_strength < 1: password_strength = 1
     return password_strength
+
+
+def get_password_blacklist(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as blacklist_file:
+            blacklist = blacklist_file.read()
+        return blacklist
+    except FileNotFoundError:
+        blacklist = None
+        return blacklist
 
 
 if __name__ == '__main__':
     console_arguments = get_console_arguments()
     password_blacklist_file = console_arguments.b
+    password_blacklist = None
+    if password_blacklist_file:
+        password_blacklist = get_password_blacklist(password_blacklist_file)
+        if password_blacklist is None:
+            print(
+                'Can not find the file containing a password blacklist. '
+                'The script will not check the password in a blacklist.'
+            )
     user_password = getpass.getpass(prompt='Password: ')
     password_score = get_password_strength(
         user_password,
-        password_blacklist_file
+        password_blacklist
     )
-    if password_score is None:
-        print('Can not find the file containing a password blacklist.')
-    else:
-        print(
-            'Your password gets {} point(s) out of 10.'.format(password_score)
-        )
+    print('Your password gets {} point(s) out of 10.'.format(password_score))
